83c0bed129b6749a2e6b8c7a070f37d6a18766c4
carol6691 <carolina.lliberos@gmail.com>
2025-02-26 16:25:36 +0100

    V1.0

---
+++ Python/phyton_ejemplo.py
@@ -0,0 +1,7 @@
+# Programa que pide una nota por consola y valora si el alumno ha aprobado o no.
+notaIn=int(input("Introduzca nota:"))
+if notaIn<5:
+    calificacion="Suspenso"
+else:
+    calificacion="Aprobado"
+print(calificacion)
