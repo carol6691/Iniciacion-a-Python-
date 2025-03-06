214d9e05899a453b11e3b88785b4686775fd27c9
carol6691 <carolina.lliberos@gmail.com>
2025-03-06 19:02:40 +0100

    V1.1

--- Python/IniciosPython2.py
+++ Python/IniciosPython2.py
@@ -1,7 +1,78 @@
-def suma():
-    num1 = 3
-    num2 = 5
-    print("suma =", num1 + num2)
-    
-
-
+n_edad = 47
+s_edad = "47"
+print (n_edad)
+print (s_edad)
+print ('hola ' + str(n_edad))
+print (33 + int(s_edad))
+print (33.5 + float(s_edad))
+print(type(n_edad))
+print(type(s_edad))
+''' next
+next'''
+A = 13
+B = "14"
+print (str(A))
+print (float(A))
+print (int(B))
+print (float(B))
+#next
+print("Esta frase","termina aquí.")
+print("Esta frase"+"termina aquí.")
+#next
+a = "uno"
+b = "dos"
+c = a + b
+print (c)
+c = a * 3
+print (c)
+#next
+texto = "HOLA"
+print (texto.lower())
+#next
+texto = "hola"
+print (texto.capitalize())
+#next
+texto = "hola"
+print (texto.count('o'))
+#next
+texto = "amor"
+print (texto.find('a'))
+#next
+texto = "loco"
+print (texto.find('o'))
+#next
+texto = "texto"
+print (texto.find('t'))
+#next
+texto = "texto"
+print (texto.rfind('t'))
+#next
+texto = "texto"
+print (texto.isdigit())
+#next
+texto = "5"
+print (texto.isdigit())
+#next
+texto = "5.5"
+print (texto.isdigit())
+#next
+texto = "texto5"
+print (texto.isdigit())
+#next
+texto = "texto"
+print (texto.isalpha())
+#next
+texto = "texto5"
+print (texto.isalpha())
+#next
+texto = "5.5"
+print (texto.isalpha())
+#next
+texto = "  texto de hola "
+print (texto.strip())
+#next
+texto = "texto de hola"
+print (texto.replace("hola", "amor"))
+#next
+edad = int(input("Introduce edad miserable:"))
+print ("Next year you'll have", edad + 1)
